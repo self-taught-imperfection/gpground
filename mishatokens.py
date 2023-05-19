0966628ad7bb98d3976d392910eeab1917f27ab6
self-taught-imperfection <124312737+self-taught-imperfection@users.noreply.github.com>
2023-05-20 02:24:07 +0300

    Update mishatokens.py

--- mishatokens.py
+++ mishatokens.py
@@ -1,5 +1,5 @@
 def chatgpt_api_key()
-    openai.api_key = ('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')
+    openai.api_key = ('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')
     return open.api_key
 def telegram_api_key()
     bot = telebot.TeleBot('5485804927:AAF8bCIXghrwvvBInILxffWx_oZ9AIbp9zI')
